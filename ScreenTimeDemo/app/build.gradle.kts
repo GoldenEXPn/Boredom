0ac879fa1a680474cf549403dd4a53ef3eedf231
Tianyi <49137737+GoldenEXPn@users.noreply.github.com>
2025-04-22 00:12:57 -0400

    Update build.gradle.kts

--- ScreenTimeDemo/app/build.gradle.kts
+++ ScreenTimeDemo/app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     
     signingConfigs {
         create("release") {
-            storeFile = file("../key.jks")
+            storeFile = file("ScreenTimeDemo/key.jks")
             storePassword = System.getenv("SIGNING_STORE_PASSWORD") ?: ""
             keyAlias = System.getenv("SIGNING_KEY_ALIAS") ?: ""
             keyPassword = System.getenv("SIGNING_KEY_PASSWORD") ?: ""
